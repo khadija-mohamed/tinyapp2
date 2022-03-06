338325355a7bd7180ca67d2f63f61333e60e882a
Khadija Mohamed <khadijam@live.ca>
2022-03-06 11:47:33 -0500

    adding route for /urls

--- express_server.js
+++ express_server.js
@@ -24,3 +24,12 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+// adding route for /urls
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+
+
