fdf8f348f6aa32b94ec57121d9ddaff403d200de
Khadija Mohamed <khadijam@live.ca>
2022-03-07 01:58:43 -0500

    updated generate function to make 6 character code

--- helpers.js
+++ helpers.js
@@ -3,7 +3,7 @@ const generateRandomString = function() {
   let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let result = ""
   let charactersLength = characters.length;
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 6; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
 }
 return result
