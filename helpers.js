23628085244ac78826360263bb31a96c04dc717a
Khadija Mohamed <khadijam@live.ca>
2022-03-07 01:45:25 -0500

    cleaned up and updated helpers functions

---
+++ helpers.js
@@ -0,0 +1,26 @@
+// generating a six-character alphanumeric code
+const generateRandomString = function() {
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = ""
+  let charactersLength = characters.length;
+  for (let i = 0; i < 5; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+}
+return result
+}
+console.log(generateRandomString(5));
+
+//get user by email stored in database
+const getUserByEmail = (database, email) => {
+  let user = {};
+
+  for (let key in database) {
+    if (database[key]['email'] === email) {
+      user = database[key];
+      return user;
+    }
+  }
+  return null;
+};
+
+module.exports = { generateRandomString, getUserByEmail };
